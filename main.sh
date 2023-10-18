9967b4e005b7980c9d7e75cad9370615c195e321
hellowjko <46467919+hellowjko@users.noreply.github.com>
2023-10-18 02:43:00 -0500

    Add files via upload

--- main.sh
+++ main.sh
@@ -22,14 +22,6 @@ for i in ${ip_all};do
 done
 
 echo -e "Product_Serial,Network_card,IP,manufacturer,product_name,RPM_System_count,RPM_Install_total,Kernel_Version,System_boot_mode,Character,network,NetworkManager,CPU_NUM,CPU_INFO,CPU_NUMA,kvm-state,Vt-d,MAX_PEF_state,P-state,C-state,Disk_Raid,Disk_Sys,Disk_Sys_Part,Disk_Info,Mem_Total,Mem_Num,Mem_Info,Net_Card_Num,Net_Info" > report/check_info.csv
-ansible-playbook check_hardware.yaml -i hosts
-file_all=`ls /tmp/work_home | grep "csv" | sort`
-for i in ${file_all};do
-    cat /tmp/work_home/${i} | sed 1d >> report/check_info.csv
-done
-sed -i 's/\t/,/g' report/check_info.csv
-rm -rf /tmp/work_home
-echo "The report has been imported into the report/check_info.csv"
 
 ansible-playbook check_hardware.yaml -i hosts
 file_all=`ls /tmp/work_home | grep "csv" | sort`
